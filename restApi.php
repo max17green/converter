69184395684883e1668d822269d014a158edc239
max17green <maximmum1994@mail.ru>
2018-10-01 12:02:52 +0400

    version 9

--- restApi.php
+++ restApi.php
@@ -1,16 +1,22 @@
 <?php
     //Запрос http://domen1/restApi.php?name=8091431
-	$name = htmlspecialchars($_GET["name"]);
-	$dir = "treatment/".$name."/images";
-	$files = scandir($dir);
-	//print_r($files);
-	foreach($files as $file) {
-		if($file == '.' || $file == '..' || is_dir($dir . $file)){
-	        continue;
-	    } else {
-	    	$arr[] = $dir."/".$file;
-	    }
+	class RestApi{
+		function getRestApi($dirName) {
+			$path = "treatment/".$dirName."/images";
+			$files = scandir($path);
+			//print_r($files);
+			foreach($files as $file) {
+				if($file == '.' || $file == '..' || is_dir($path . $file)){
+			        continue;
+			    } else {
+			    	$arr[] = $path."/".$file;
+			    }
+			}
+			return json_encode($arr);
+		}
 	}
-	//print_r($arr);
-	echo json_encode($arr);
+	$name = htmlspecialchars($_GET["name"]);
+	$rest = new RestApi();
+	$str = $rest->getRestApi($name);
+	echo $str;
 ?>
