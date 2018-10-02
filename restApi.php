8899fa02fbd54e9b6eb30b51c802a31beb3491ac
max17green <maximmum1994@mail.ru>
2018-10-02 23:05:36 +0400

    version 11

--- restApi.php
+++ restApi.php
@@ -1,22 +1,11 @@
 <?php
-    //Запрос http://domen1/restApi.php?name=8091431
-	class RestApi{
-		function getRestApi($dirName) {
-			$path = "treatment/".$dirName."/images";
-			$files = scandir($path);
-			//print_r($files);
-			foreach($files as $file) {
-				if($file == '.' || $file == '..' || is_dir($path . $file)){
-			        continue;
-			    } else {
-			    	$arr[] = $path."/".$file;
-			    }
-			}
-			return json_encode($arr);
-		}
-	}
-	$name = htmlspecialchars($_GET["name"]);
-	$rest = new RestApi();
-	$str = $rest->getRestApi($name);
-	echo $str;
-?>
+namespace RestApiClass;
+
+require_once("treatment/classes/RestApiClass.php");
+
+//Запрос http://domen1/restApi.php?name=8091431
+
+$name = htmlspecialchars($_GET["name"]);
+$rest = new RestApi();
+$str = $rest->getRestApi($name);
+echo $str;
--- treatment/zip.php
+++ treatment/zip.php
@@ -1,52 +1,7 @@
 <?php
-class ArhiveClass{
-    private $zip;
-    public $pathdir;
-    private $nameArhive;
-    function init($par) {
+namespace Arhive;
 
-        if (!empty($par) && $par != null) {
-            if (file_exists($par)) {
-                $this->pathdir = htmlspecialchars($par); // путь к папке, файлы которой будем архивировать
-                $this->nameArhive = time().'.zip'; //название архива
-                $this->zip = new ZipArchive; // класс для работы с архивами
-                //echo $this->pathdir;
-                return true;
-            }
-        }
-        
-    }
-    function create() {
-        return $this->zip -> open($this->nameArhive, ZipArchive::CREATE);
-    }
-    function addIn() {
-        $dir = opendir($this->pathdir); // открываем папку с файлами
-        //echo "<pre>";
-        while( $file = readdir($dir)){ // перебираем все файлы из нашей папки
-
-                if($file == '.' || $file == '..' || is_dir($this->pathdir."/".$file)){
-                    continue;
-                } else {
-                    //echo $pathdir."/".$file."\n";
-                    $this->zip -> addFile($this->pathdir."/".$file, $file); // и архивируем
-                    $this->zip -> addFile($this->pathdir."/index.html","index.html");
-                    $this->zip -> addGlob("assets/*.*");
-                    $this->zip -> addGlob("assets/fonts/*.*");
-                    $this->zip -> addGlob($this->pathdir."/images/*.jpg");
-                }
-        }
-
-        $this->zip -> close(); // закрываем архив.
-    }
-    function output() {
-        header('Content-type: application/zip');
-        header('Content-Disposition: attachment; filename="'.$this->nameArhive.'"');
-        readfile($this->nameArhive);
-    }
-    function printError() {
-        die ('<div class="container">Произошла ошибка при создании архива</div>');
-    }
-}
+require_once("classes/Arhive.php");
 
 $a = new ArhiveClass();
 if ($a->init($_GET["name"])) {
@@ -54,6 +9,9 @@ if ($a->init($_GET["name"])) {
         //echo "верно";
         $a->addIn();
         $a->output();
-    } else $a->printError();
-} else $a->printError();
-?>
+    } else {
+        $a->printError();
+    }
+} else {
+    $a->printError();
+}
